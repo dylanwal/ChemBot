ff028ab2ec75673d45f57f9e1125a713948d38c8
dylanwal <dylanwal@mit.edu>
2023-04-14 07:24:58 -0400

    adding gui

---
+++ chembot/gui/tabs/tab1_callback.py
@@ -0,0 +1,24 @@
+from dash import html, dcc
+
+from chembot.gui.main import app
+from dash.dependencies import Input, Output, State
+
+
+@app.callback(Output('send-message-status', 'children'),
+              Input('send-button', 'n_clicks'),
+              State('message-input', 'value'))
+def send_message_to_rabbitmq(n_clicks, message):
+    if n_clicks > 0:
+        try:
+            # connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+            # channel = connection.channel()
+            # channel.queue_declare(queue='my_queue')
+            # channel.basic_publish(exchange='', routing_key='my_queue', body=message)
+            # connection.close()
+            return html.Div('Message sent to RabbitMQ')
+        except:
+            return html.Div('Error: Could not connect to RabbitMQ')
+    else:
+        return html.Div()
+
+
---
+++ chembot/gui/tabs/tab3_layout.py
@@ -0,0 +1,8 @@
+
+from dash import html, dcc
+
+tab3_layout = html.Div([
+    html.H1('Page 3'),
+    html.Div(id='page-3-content')
+])
+
