8b9f90a26852e650c711746db346a0f3e84e9cdc
dylanwal <dylanwal@mit.edu>
2023-07-11 11:23:25 -0400

    bug fix atir

--- runs/simple_setup/schedule_atir.py
+++ runs/simple_setup/schedule_atir.py
@@ -13,12 +13,12 @@ def job_rotate_through_all_positions() -> JobSequence:
             Event(
                 resource=NamesSensors.ATIR,
                 callable_=ATIR.write_background,
-                duration=timedelta(seconds=15),
+                duration=timedelta(seconds=20),
             ),
             Event(
                 resource=NamesSensors.ATIR,
                 callable_=ATIR.write_measure,
-                duration=timedelta(seconds=15),
+                duration=timedelta(seconds=20),
             )
         ]
     )
